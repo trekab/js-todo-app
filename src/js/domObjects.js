bae1ce4c3880210580ac21090b441f884e5ffd10
aecordova <ae_cordova@outlook.com>
2020-05-25 19:30:47 -0500

    Move DOM List item creator to its own module.

---
+++ src/js/domObjects.js
@@ -0,0 +1,25 @@
+
+const ListItem = (item) => {
+  const li = document.createElement('li');
+  const liTitle = document.createElement('input');
+  const liCheck = document.createElement('i');
+
+  li.classList = 'list-group-item d-flex justify-content-between align-items-center';
+  liCheck.classList = 'far fa-circle d-block';
+  liTitle.classList = 'w-100 item-title';
+
+  liTitle.type = 'text';
+  liTitle.placeholder = 'To do...';
+
+  liCheck.addEventListener('click', () => {
+    liCheck.classList.toggle('fa-check-circle');
+    liCheck.classList.toggle('fa-circle');
+  });
+
+  li.appendChild(liTitle);
+  li.appendChild(liCheck);
+
+  return li;
+};
+
+export default { ListItem };
