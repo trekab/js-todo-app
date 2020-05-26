67bc02071bc6d19ce07d13245e712f00a96473cb
Treasure Kabareebe <treasurecalm@gmail.com>
2020-05-26 23:06:27 +0300

    Add input field for title plus corresponding icons through JS

--- src/js/domObjects.js
+++ src/js/domObjects.js
@@ -2,25 +2,44 @@
 const ListItem = (item) => {
   const li = document.createElement('li');
   const liTitle = document.createElement('input');
+  const liTitleDiv = document.createElement('div');
   const liCheck = document.createElement('i');
+  const liDelete = document.createElement('i');
   const liCaret = document.createElement('i');
+  const toggleDetails = document.createElement('a');
+  //const toggleDetailsIcon = document.createElement('i');
 
-  li.classList = 'list-group-item d-flex justify-content-between px-1 align-items-center';
-  liCheck.classList = 'far fa-circle d-none';
-  liCaret.classList = 'fas fa-chevron-down d-block';
+  toggleDetails.classList = 'p-1 d-block';
+  toggleDetails.href = '#item-details-form';
+  toggleDetails.role = 'button';
+  toggleDetails['aria-expanded'] = 'false';
+  toggleDetails['aria-controls'] = 'item-details-form';
+  //toggleDetailsIcon.classList = 'fas fa-chevron-down d-block mr-1';
+  toggleDetails.appendChild(liCaret);
+
+  liTitleDiv.classList = 'd-flex justify-content-between align-items-center';
+  liDelete.classList = 'fas fa-times';
+  li.classList = 'list-group-item';
+  liCheck.classList = 'far fa-circle m-2';
+  liCaret.classList = 'fas fa-chevron-down d-block m-2';
   liTitle.classList = 'w-100 item-title';
 
   liTitle.type = 'text';
-  liTitle.placeholder = 'To do...';
+  liTitle.placeholder = 'Enter task title...';
 
   liCheck.addEventListener('click', () => {
     liCheck.classList.toggle('fa-check-circle');
     liCheck.classList.toggle('fa-circle');
   });
 
-  li.appendChild(liTitle);
-  li.appendChild(liCaret);
-  li.appendChild(liCheck);
+  liTitleDiv.appendChild(liCheck);
+  liTitleDiv.appendChild(liTitle);
+  liTitleDiv.appendChild(liDelete);
+  liTitleDiv.appendChild(toggleDetails);
+
+  
+
+  li.appendChild(liTitleDiv);
 
   const startEdit = () => {
     setTimeout(() => {
