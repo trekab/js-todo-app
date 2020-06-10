e5135277c06e80ab7352f26da94227ca069fe8a4
aecordova <ae_cordova@outlook.com>
2020-06-10 13:55:29 -0500

    Add create Project functionality

--- src/index.js
+++ src/index.js
@@ -3,40 +3,19 @@ import './css/main.scss';
 import todoItems from './js/todoItem';
 import domItems from './js/domObjects';
 
-const allToDos = todoItems.List;
 const projectList = todoItems.List;
 projectList.name = 'Projects';
 
 const sideBar = document.getElementById('left-sidebar');
-const sideBarBtn = document.getElementById('new-btn-set');
-sideBarBtn.addEventListener('click', () => {
-  domItems.ProjectList(projectList, projectList.name );
+sideBar.appendChild(domItems.ProjectList(projectList));
+
+const title = document.querySelector('h3');
+
+title.addEventListener('click', ()=>{
+  console.log(projectList.items);
+
 })
-// sideBar.appendChild(domItems.ProjectList(projectList, projectList.name ));
-//allToDos.name = 'All';
 
-const main = document.getElementById('main-section');
-const upcoming = document.querySelector('.btn-upcoming');
-upcoming.addEventListener('click', () => {
-  main.innerHTML = '';
-});
-
-
-//main.appendChild(domItems.TodoList(allToDos));
-
-// const listGroup = document.getElementById('list-group');
-// const addTodo = document.getElementById('add-todo');
-const btn = document.querySelector('.btn-all');
-
-btn.addEventListener('click', () => {
-  allToDos.name = 'All';
-  main.innerHTML = '';
-  main.appendChild(domItems.TodoList(allToDos));
-});
-
-// addTodo.addEventListener('click', () => {
-//   const newToDo = todoItems.Item;
-//   const li = domItems.TodoListItem(newToDo, allToDos);
-//   listGroup.insertBefore(li.li, listGroup.firstChild);
-//   li.startEdit();
-// });
+// allToDos.name = 'All';
+// main.innerHTML = '';
+// main.appendChild(domItems.TodoList(allToDos));
